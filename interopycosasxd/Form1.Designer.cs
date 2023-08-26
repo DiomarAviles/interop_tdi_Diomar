d0112f2a4783b78dfabdd800e1c1edc16ae86e21
isaia <isaia@DESKTOP-QDSFA8K>
2023-08-26 11:04:35 -0600

    Agregar archivos de proyecto.

---
+++ interopycosasxd/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace interopycosasxd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtdato = new System.Windows.Forms.TextBox();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtdato
+            // 
+            this.txtdato.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdato.Location = new System.Drawing.Point(82, 125);
+            this.txtdato.Name = "txtdato";
+            this.txtdato.Size = new System.Drawing.Size(312, 30);
+            this.txtdato.TabIndex = 0;
+            this.txtdato.Text = "Hola mundo!";
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnguardar.Location = new System.Drawing.Point(420, 125);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(196, 30);
+            this.btnguardar.TabIndex = 1;
+            this.btnguardar.Text = "Guardar En Word";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(420, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Guardar En excel ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.txtdato);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtdato;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ interopycosasxd/Form1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace interopycosasxd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dailo = new SaveFileDialog(); 
+            if(dailo.ShowDialog()!= DialogResult.OK)
+            {
+                return;
+            }
+            string ruta = dailo.FileName;
+            var wordApp = new Word.Application();
+            wordApp.Visible = true;
+            wordApp.Documents.Add();
+            string dato = txtdato.Text;
+            wordApp.Selection.TypeText(dato);
+            wordApp.ActiveDocument.SaveAs2(ruta);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dailo = new SaveFileDialog();
+            if (dailo.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            string ruta = dailo.FileName;
+            var excelApp = new Excel.Application();
+            // Add a new Excel workbook.
+            excelApp.Workbooks.Add();
+            excelApp.Visible = true;
+            string dato = txtdato.Text;
+            excelApp.Range["A1"].Value = dato;
+            excelApp.ActiveWorkbook.SaveAs(ruta);
+            excelApp.Columns[1].AutoFit();c
+        }
+    }
+}
